c1f7f1e352258d4885ad69140560f66affff5a4c
Libert R Schmidt <katari@waynasolutions.com>
2016-05-30 16:48:38 -0400

    displayOk

--- booklos/views.py
+++ booklos/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-
+from models import books
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 
 def book_list(request):
-    return render(request, "index.html")
+
+    query_list = books.objects.all()
+    paginator = Paginator(query_list, 7)
+
+    page = request.GET.get('page')
+    try:
+        query= paginator.page(page)
+    except PageNotAnInteger:
+        query = paginator.page(1)
+    except EmptyPage:
+        query = paginator.page(paginator.num_pages)
+    context = {'query':query}
+
+    return render(request, "index.html",context)
